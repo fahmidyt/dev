d717da0cb41a31849af13e543dc70ad6ce02a135
fahmidyt <fmidyt@gmail.com>
2024-11-18 23:53:17 +0700

    Initialize frontend and backend structure with essential configurations and files

---
+++ backend/lib/backend_web/router.ex
@@ -0,0 +1,21 @@
+defmodule BackendWeb.Router do
+  use BackendWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BackendWeb do
+    pipe_through :api
+  end
+
+  # Enable Swoosh mailbox preview in development
+  if Application.compile_env(:backend, :dev_routes) do
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
